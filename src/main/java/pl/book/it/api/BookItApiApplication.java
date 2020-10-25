c20000d9c42ed68b35e59f62a39a4e4f37181a73
Matylda <matylda.badowska@gmail.com>
2020-10-25 17:16:18 +0100

    BIA-1 project initialization

---
+++ src/main/java/pl/book/it/api/BookItApiApplication.java
@@ -0,0 +1,13 @@
+package pl.book.it.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookItApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookItApiApplication.class, args);
+	}
+
+}
