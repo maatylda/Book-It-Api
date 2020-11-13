0573c255a863d865474dabe132c4d2a6630bbfab
Matylda <matylda.badowska@gmail.com>
2020-11-13 16:13:53 +0100

    BIA-8 refactoring services

--- src/main/java/pl/book/it/api/services/user/UserService.java
+++ src/main/java/pl/book/it/api/services/user/UserService.java
@@ -1,16 +1,14 @@
 package pl.book.it.api.services.user;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.book.it.api.domain.User;
 import pl.book.it.api.exceptions.BookItException;
-import pl.book.it.api.exceptions.NoUserWithGivenIdException;
+import pl.book.it.api.model.ApiErrors;
 import pl.book.it.api.model.Dto.UserDto;
 import pl.book.it.api.model.user.specifications.Role;
 import pl.book.it.api.repositories.UserRepository;
-import pl.book.it.api.services.validation.BookingValidator;
 
 @Service
 @RequiredArgsConstructor
@@ -19,23 +17,19 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
-    private final BookingValidator bookingValidator;
 
     public boolean isThereAnAccountWithGivenEmail(String email) {
         return userRepository.findById(email).isPresent();
     }
 
     public User findUserById(String email) {
-        if (isThereAnAccountWithGivenEmail(email)) {
-            return userRepository.getOne(email);
-        }
-        throw new NoUserWithGivenIdException("There is no user with email: " + email);
+        return userRepository.findById(email).orElseThrow(() ->
+                new BookItException("There is no user with email: " + email, ApiErrors.USER_NOT_FOUND.getCode()));
     }
-
 
     public User createUser(UserDto userDto, Role role) {
         if (isThereAnAccountWithGivenEmail(userDto.getEmail())) {
-            throw new BookItException(400, String.format("There is already user with email: %s, chose another email.", userDto.getEmail()), 101);
+            throw new BookItException(String.format("There is already user with email: %s, chose another email.", userDto.getEmail()), 101);
         }
         User user = userMapper.createUser(userDto);
         userMapper.setRoleForUser(role, user);
@@ -43,16 +37,12 @@ public class UserService {
         return user;
     }
 
-
     public void updateUser() {
-
         //todo
-
     }
 
     public void deleteUser(User user) {
         userRepository.delete(user);
     }
-
 
 }
