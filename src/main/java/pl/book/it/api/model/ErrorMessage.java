b228766211b1c576d029d0e068706e894a757119
Matylda <matylda.badowska@gmail.com>
2020-11-13 22:59:26 +0100

    BIA-10 Handle MethodArrgumentNotValidException

--- src/main/java/pl/book/it/api/model/ErrorMessage.java
+++ src/main/java/pl/book/it/api/model/ErrorMessage.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,5 +16,16 @@ public class ErrorMessage {
     @JsonProperty("msg")
     private String message;
     private Integer code;
+    private Map<String, String> errors;
+
+    public ErrorMessage(Integer httpStatus, String message, Integer code) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+        this.code = code;
+    }
+
+    public ErrorMessage(Map<String, String> errors) {
+        this.errors = errors;
+    }
 }
 
--- src/main/java/pl/book/it/api/web/GlobalExceptionHandler.java
+++ src/main/java/pl/book/it/api/web/GlobalExceptionHandler.java
@@ -1,12 +1,18 @@
 package pl.book.it.api.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import pl.book.it.api.annotations.HandledByBookItExceptionHandler;
 import pl.book.it.api.exceptions.BookItException;
 import pl.book.it.api.model.ErrorMessage;
+
+import java.util.HashMap;
+import java.util.List;
 
 @RestControllerAdvice(annotations = {HandledByBookItExceptionHandler.class})
 @Slf4j
@@ -21,5 +27,16 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(exp.getStatus()).body(errorMessage);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> handleMethodArgumentNotValidException(final MethodArgumentNotValidException exp) {
+        final HashMap<String, String> errorMessagesByFieldName = new HashMap<>();
+
+        final List<FieldError> fieldErrors = exp.getBindingResult().getFieldErrors();
+        for (FieldError fieldError : fieldErrors) {
+            errorMessagesByFieldName.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorMessage(errorMessagesByFieldName));
+    }
 
 }
