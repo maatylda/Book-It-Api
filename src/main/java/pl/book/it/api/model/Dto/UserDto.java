a02761c3bb28ec4b8135cd7989f1b97c1d9b4287
Matylda <matylda.badowska@gmail.com>
2020-11-17 20:51:39 +0100

    BIA-14 reformat

--- src/main/java/pl/book/it/api/model/Dto/UserDto.java
+++ src/main/java/pl/book/it/api/model/Dto/UserDto.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
-import pl.book.it.api.annotations.EmailNotTaken;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
